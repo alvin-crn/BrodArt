4f3ac61dfba077c71fad8626949dedddc2ebc928
Alvin CARNEIRO <alvin.carneiro1@gmail.com>
2026-01-15 14:30:08 +0100

    Bestseller + nouveauté + catégories
    Création des pages nouveautés, meilleures ventes, et catégories

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -62,4 +62,116 @@ class ProductController extends AbstractController
         ]);
     }
 
+    #[Route('/meilleure-vente', name: 'best_sale')]
+    public function bestSale(Request $request): Response
+    {
+        $search = New SearchService;
+        $formSearch = $this->createForm(SearchType::class, $search);
+
+        $formSearch->handleRequest($request);
+        
+        if ($formSearch->isSubmitted() && $formSearch->isValid()) {
+            $allProducts = $this->em->getRepository(Product::class)->findWithSearch($search);
+            foreach ($allProducts as $product) {
+                if ($product->getIsBest() == true){
+                    $bestProducts[] = $product;
+                }
+            }
+
+            if (empty($bestProducts)) {
+                $bestProducts = [];
+            }
+        } else {
+            $bestProducts = $this->em->getRepository(Product::class)->findByIsBest(1);
+
+            if (empty($bestProducts)) {
+                $bestProducts = [];
+            }
+        }
+
+        return $this->render('product/index.html.twig', [
+            'products' => $bestProducts,
+            'formSearch' => $formSearch->createView(),
+        ]);
+    }
+
+    #[Route('/nouveauté', name: 'new_product')]
+    public function newProduct(Request $request): Response
+    {
+        $search = New SearchService;
+        $formSearch = $this->createForm(SearchType::class, $search);
+
+        $formSearch->handleRequest($request);
+
+        $now = new DateTimeImmutable();
+
+        if ($formSearch->isSubmitted() && $formSearch->isValid()) {
+            $allProducts = $this->em->getRepository(Product::class)->findWithSearch($search);
+            foreach ($allProducts as $product) {
+                if ($now < $product->getCreatedAt()->modify('+ 1 month')) {
+                    $newProducts[] = $product;
+                }
+            }
+
+            if (empty($newProducts)) {
+                $newProducts = [];
+            }
+        } else {
+            $allProducts = $this->em->getRepository(Product::class)->findAll();
+            foreach ($allProducts as $product) {
+                if ($now < $product->getCreatedAt()->modify('+ 1 month')) {
+                    $newProducts[] = $product;
+                }
+            }
+
+            if (empty($newProducts)) {
+                $newProducts = [];
+            }
+        }
+
+        return $this->render('product/index.html.twig', [
+            'products' => $newProducts,
+            'formSearch' => $formSearch->createView(),
+        ]);
+    } 
+
+    #[Route('/categorie/{category}', name: 'product_by_category')]
+    public function categorie($category, Request $request): Response
+    {
+        $categorie = $this->em->getRepository(Category::class)->findOneByName($category);
+
+        if(!$categorie) {
+            return $this->redirectToRoute('products');
+        }
+
+        $search = New SearchService;
+        $formSearch = $this->createForm(SearchType::class, $search);
+
+        $formSearch->handleRequest($request);
+        
+        if ($formSearch->isSubmitted() && $formSearch->isValid()) {
+            $allProducts = $this->em->getRepository(Product::class)->findWithSearch($search);
+            foreach ($allProducts as $product) {
+                if ($product->getCategory()->getId() == $categorie->getId()) {
+                    $productByCategory[] = $product;
+                }
+            }
+
+            if (empty($productByCategory)) {
+                $productByCategory = [];
+            }
+        } else {
+            $productByCategory = $this->em->getRepository(Product::class)->findByCategory($categorie->getId());
+
+            if (empty($productByCategory)) {
+                $productByCategory = [];
+            }
+        }
+
+        return $this->render('product/index.html.twig', [
+            'notCategory' => 'notCategory',
+            'products' => $productByCategory,
+            'formSearch' => $formSearch->createView(),
+        ]);
+    }
 }
